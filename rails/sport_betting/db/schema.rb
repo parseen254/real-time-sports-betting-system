2d2c5ac0fe0f4226d0b13a47ef1adaab88bcc18b
David Parseen <david.parseen@gmail.com>
2025-03-04 16:29:03 +0300

    chore: Create migrations for game and bets

--- rails/sport_betting/db/schema.rb
+++ rails/sport_betting/db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_04_123219) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_04_132539) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bets", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "game_id", null: false
+    t.decimal "amount"
+    t.integer "odds"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_bets_on_game_id"
+    t.index ["user_id"], name: "index_bets_on_user_id"
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.decimal "odds"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +45,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_04_123219) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bets", "games"
+  add_foreign_key "bets", "users"
 end
